4721a25cebdf05102de93c223d1370abb008b32f
mina karam <minakaramkhalil.28@gmail.com>
2023-11-13 23:59:40 +0200

    a

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
@@ -11,65 +11,79 @@ const Tab = createBottomTabNavigator();
 const Router = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBarOptions={{
-      activeTintColor: 'white',
-      activeBackgroundColor:'black',
-      inactiveBackgroundColor:'black',
-      inactiveTintColor: 'gray', 
-        labelStyle: {
-          fontSize: 12,
-          fontWeight: 'bold',
-        },
-        tabBarStyle: {
-          borderTopColor: '#222',
-          borderTopWidth: 1,
-          backgroundColor: '#222',
-        },
-        headerShown:false,
-      }}
-      tabBarOptions={{
-        activeTintColor: 'white',
-        inactiveTintColor: 'gray', 
-      }}>
-        <Tab.Screen name="Home" component={Home} options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="home-outline"
-              size={size}
-              color={color}
-            />
-          ),
-          tabBarLabel: 'Explore',
-          tabBarLabelPosition: 'below-icon',
-          headerShown:false,
-        }}/>  
-        <Tab.Screen name="Settings" component={Search} options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="book-search-outline"
-              size={size}
-              color={color}
-            />
-          ),
-          tabBarLabel: 'Search',
-          tabBarLabelPosition: 'below-icon',
-          headerShown:false,
-        }} />
-        <Tab.Screen name="Favourites" component={Favourites} options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="heart-outline"
-              size={size}
-              color={color}
-            />
-          ),
-          tabBarLabel: 'Favourites',
-          tabBarLabelPosition: 'below-icon',
-          headerShown:false,
-        }}/>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: 'white',
+          activeBackgroundColor: 'black',
+          inactiveBackgroundColor: 'black',
+          inactiveTintColor: 'gray',
+          labelStyle: {
+            fontSize: 12,
+            fontWeight: 'bold',
+          },
+          tabBarStyle: {
+            borderTopColor: '#222',
+            borderTopWidth: 1,
+            backgroundColor: '#222',
+          },
+          headerShown: false,
+        }}
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="home-outline"
+                size={size}
+                color={color}
+              />
+            ),
+            tabBarLabel: 'Explore',
+            tabBarLabelPosition: 'below-icon',
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Search}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="book-search-outline"
+                size={size}
+                color={color}
+              />
+            ),
+            tabBarLabel: 'Search',
+            tabBarLabelPosition: 'below-icon',
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Favourites"
+          component={Favourites}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="heart-outline"
+                size={size}
+                color={color}
+              />
+            ),
+            tabBarLabel: 'Favourites',
+            tabBarLabelPosition: 'below-icon',
+            headerShown: false,
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
