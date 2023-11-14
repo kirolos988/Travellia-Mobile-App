fd9b8bc4f0943c9a974c0393513f640c238fe13f
mina karam <minakaramkhalil.28@gmail.com>
2023-11-15 01:07:59 +0200

    push

--- src/components/SinglePage/SinglePage.jsx
+++ src/components/SinglePage/SinglePage.jsx
@@ -12,17 +12,16 @@ import {
 } from 'react-native';
 
 const SinglePage = () => {
-    const item = useRoute().params
-
-    console.log(item.data.images)
+  const categoryData = useRoute().params;
+  const { images, name, rating, reviews, phone, website } = categoryData.data;
+  console.log(images);
   const flatlistRef = useRef();
-  const [data, setData] = useState([]);
   const screenWidth = Dimensions.get('window').width;
   const [activeIndex, setActiveIndex] = useState(0);
   //Auto Scroll
   useEffect(() => {
     let interval = setInterval(() => {
-      if (activeIndex === item.data.images.length - 1) {
+      if (activeIndex === images.length - 1) {
         flatlistRef.current.scrollToIndex({ index: 0, animation: true });
       } else {
         flatlistRef.current.scrollToIndex({
@@ -32,76 +31,70 @@ const SinglePage = () => {
       }
     }, 2000);
     return () => clearInterval(interval);
-  });
+  }, []);
 
   const getItemLayout = (data, index) => ({
     length: screenWidth,
     offset: screenWidth * index,
     index: index,
   });
- 
-  const renderItem = ({ item, index }) => {
-    return (
-      <View>
-        <Image
-          source={item}
-          style={{ height: 200, width: screenWidth }}
-        />
-      </View>
-    );
-  };
 
   const handleScroll = (event) => {
     const scrollPosition = event.nativeEvent.contentOffset.x;
-    console.log({ scrollPosition });
+    // console.log({ scrollPosition });
     const index = scrollPosition / screenWidth;
     // console.log({ index });
     setActiveIndex(index);
   };
-  const renderDotIndicators = () => {
-    return item.data.images.map((dot, index) => {
-      if (activeIndex === index) {
-        return (
-          <View
-            key={index}
-            style={{
-              backgroundColor: 'yellow',
-              width: 10,
-              height: 10,
-              borderRadius: 5,
-              marginHorizontal: 6,
-            }}
-          ></View>
-        );
-      } else {
-        return (
-          <View
-            key={index}
-            style={{
-              backgroundColor: 'white',
-              width: 10,
-              height: 10,
-              borderRadius: 5,
-              marginHorizontal: 6,
-            }}
-          ></View>
-        );
-      }
-    });
-  };
+  // const renderDotIndicators = () => {
+  //   return item.data.images.map((dot, index) => {
+  //     if (activeIndex === index) {
+  //       return (
+  //         <View
+  //           key={index}
+  //           style={{
+  //             backgroundColor: 'yellow',
+  //             width: 10,
+  //             height: 10,
+  //             borderRadius: 5,
+  //             marginHorizontal: 6,
+  //           }}
+  //         ></View>
+  //       );
+  //     } else {
+  //       return (
+  //         <View
+  //           key={index}
+  //           style={{
+  //             backgroundColor: 'white',
+  //             width: 10,
+  //             height: 10,
+  //             borderRadius: 5,
+  //             marginHorizontal: 6,
+  //           }}
+  //         ></View>
+  //       );
+  //     }
+  //   });
+  // };
   return (
-    <View style={{ backgroundColor: 'black ]', flex: 1 }}>
+    <View style={{ backgroundColor: '#181818', flex: 1 }}>
       <FlatList
-        data={item.data.images}
+        style={{ flexGrow: 0 }}
+        data={images}
         ref={flatlistRef}
         getItemLayout={getItemLayout}
-        renderItem={renderItem}
+        renderItem={({ item }) => (
+          <View style={{ height: 300 }}>
+            <Image source={item} style={{ flex: 1, width: screenWidth }} />
+          </View>
+        )}
         keyExtractor={(item) => item.id}
         horizontal={true}
         onScroll={handleScroll}
         pagingEnabled={true}
       ></FlatList>
-      <View
+      {/* <View
         style={{
           flexDirection: 'row',
           justifyContent: 'center',
@@ -109,29 +102,49 @@ const SinglePage = () => {
         }}
       >
         {renderDotIndicators()}
-      </View>
-      {/* <View>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>Hilton hotel</Text>
-        <Text> 9.6 Rating</Text>
-       
-        <View style={{flexDirection:"row"}}>
-          <Text style={{textDecorationLine:"underline"}} onPress={() => Linking.openURL("http://google.com")}>Visit website </Text>
-          <Text style= {{marginHorizontal:10,textDecorationLine:"underline"}}>+103 330 4433 </Text>
-          <Text style={{textDecorationLine:"underline"}}>Write a reviews </Text>
+      </View> */}
+      <View style={{ flex: 1 }}>
+        <Text style={styles.name}>{name}</Text>
+        <Text> {rating}</Text>
+
+        <View style={{ flexDirection: 'row' }}>
+          <Text
+            style={{ textDecorationLine: 'underline', color: 'white' }}
+            onPress={() => Linking.openURL({ website })}
+          >
+            Visit website
+          </Text>
+          <Text
+            style={{
+              marginHorizontal: 10,
+              textDecorationLine: 'underline',
+              color: 'white',
+            }}
+          >
+            {phone && phone}
+          </Text>
+          <Text style={{ textDecorationLine: 'underline', color: 'white' }}>
+            Write a reviews
+          </Text>
         </View>
-        
       </View>
-      
 
       <View style={styles.fixedButton}>
-      <Text style={styles.fixedButtonText}>Fixed Button</Text>
-    </View> */}
+        <Text style={styles.fixedButtonText}>Fixed Button</Text>
+      </View>
     </View>
   );
 };
 
 export default SinglePage;
 const styles = StyleSheet.create({
+  name: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    paddingVertical: 10,
+  },
+
   // fixedButton: {
   //     position: 'absolute',
   //     bottom: 0,
