cc0f6a9a5ff24730529fb3bab71459d4d98d3331
mina karam <minakaramkhalil.28@gmail.com>
2023-11-19 19:57:46 +0200

    a

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,17 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import CatButton from '../components/CategoryButton';
-import Nearby from '../components/Nearby';
+import CatButton from '../components/CategoryButton'; 
 
 const Search = () => {
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View style={{ flex: 1 }}>
         <CatButton />
-      </View>
-      <View>
-        <Nearby />
       </View>
     </SafeAreaView>
   );
